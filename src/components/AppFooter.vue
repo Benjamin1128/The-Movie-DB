c8c2e73d28957557d176de70f001a115db5716c2
Benjamin1128 <benjaminyeelc@yahoo.com>
2024-11-05 15:45:12 +0800

    test2

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,12 +2,14 @@
   <div class="footer-container">
     <div class="row">
       <div class="logo-column-content">
-        <img src="../assets/footerLogo.svg" 
-        alt="footerTMDB Logo"
-        class="footer-logo">
+        <img
+          src="../assets/footerLogo.svg"
+          alt="footerTMDB Logo"
+          class="footer-logo"
+        />
         <br />
         <q-btn no-caps unelevated class="joinCom wrap-text">
-          {{ joinCommunityBt
+          {{ joinCommunityBtn }}
         </q-btn>
       </div>
 
@@ -21,28 +23,38 @@
             <q-item-section class="category-content">Contact Us</q-item-section>
           </q-item>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">Support Forums</q-item-section>
+            <q-item-section class="category-content"
+              >Support Forums</q-item-section
+            >
           </q-item>
           <q-item clickable v-ripple>
             <q-item-section class="category-content">API</q-item-section>
           </q-item>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">System Status</q-item-section>
+            <q-item-section class="category-content"
+              >System Status</q-item-section
+            >
           </q-item>
         </q-list>
       </div>
-      
+
       <div class="header-column-content">
         <q-list class="menu-list">
           <label class="category-header">GET INVOLVED</label>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">Contribution Bible</q-item-section>
+            <q-item-section class="category-content"
+              >Contribution Bible</q-item-section
+            >
           </q-item>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">Add New Movie</q-item-section>
+            <q-item-section class="category-content"
+              >Add New Movie</q-item-section
+            >
           </q-item>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">Add New TV Show</q-item-section>
+            <q-item-section class="category-content"
+              >Add New TV Show</q-item-section
+            >
           </q-item>
         </q-list>
       </div>
@@ -54,10 +66,14 @@
             <q-item-section class="category-content">Guidelines</q-item-section>
           </q-item>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">Discussions</q-item-section>
+            <q-item-section class="category-content"
+              >Discussions</q-item-section
+            >
           </q-item>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">Leaderboard</q-item-section>
+            <q-item-section class="category-content"
+              >Leaderboard</q-item-section
+            >
           </q-item>
         </q-list>
       </div>
@@ -66,16 +82,24 @@
         <q-list class="menu-list">
           <label class="category-header">LEGAL</label>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">Terms of Use</q-item-section>
+            <q-item-section class="category-content"
+              >Terms of Use</q-item-section
+            >
           </q-item>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">API Terms of Use</q-item-section>
+            <q-item-section class="category-content"
+              >API Terms of Use</q-item-section
+            >
           </q-item>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">Privacy Policy</q-item-section>
+            <q-item-section class="category-content"
+              >Privacy Policy</q-item-section
+            >
           </q-item>
           <q-item clickable v-ripple>
-            <q-item-section class="category-content">DMCA Policy</q-item-section>
+            <q-item-section class="category-content"
+              >DMCA Policy</q-item-section
+            >
           </q-item>
         </q-list>
       </div>
@@ -84,29 +108,24 @@
 </template>
 
 <script>
-
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-    name: 'FooterTMDB',
-    computed: {
-      ...mapGetters("userdata", [
-        'getUserLogin',
-        'getUsername',
-      ]),
-      joinCommunityBtn() {
-        if (this.getUserLogin) {
-          return `Hi ${this.getUsername}!`
-        } else {
-          return "JOIN THE COMMUNITY";
-        }
+  name: "FooterTMDB",
+  computed: {
+    ...mapGetters("userdata", ["getUserLogin", "getUsername"]),
+    joinCommunityBtn() {
+      if (this.getUserLogin) {
+        return `Hi ${this.getUsername}!`;
+      } else {
+        return "JOIN THE COMMUNITY";
       }
-    }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .footer-container {
   background-color: #031d33;
   height: 320px;
@@ -127,7 +146,7 @@ export default {
   margin-top: 10px;
 }
 
-.footer-logo { 
+.footer-logo {
   height: 100px;
   width: auto;
 }
@@ -138,7 +157,6 @@ export default {
   padding-top: 30px;
   padding-right: 30px;
 }
-
 
 .header-column-content {
   flex: 1 1 18%;
